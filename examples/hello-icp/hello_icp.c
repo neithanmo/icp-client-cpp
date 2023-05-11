ad4dc3000d16e9f0965a9eab711202f0fa526cce
Carlos Medeiros <carlos.medeiros@zondax.ch>
2023-05-11 18:05:43 +0100

    update example and bindings update

--- examples/hello-icp/hello_icp.c
+++ examples/hello-icp/hello_icp.c
@@ -54,31 +54,20 @@ removing the need for global variables.
 #include "helper.h"
 #include "agent.h"
 
-#define ANONYMOUS_ID_LEN 1
+#define CHECK_ERROR(e)              \
+        if (e.len > 0) {            \
+            printf("%s\n", e.ptr);   \
+            return ERR;              \
+        }                           \
 
-// Structures to save returns from function pointer and use throughout the code
 Error error;
-Principal principal;
 Identity id;
-Text text;
 
 // Function pointers used to get the return from rust lib
 static void error_cb(const uint8_t *p, int len) {
     error.ptr = malloc(len);
     error.len = len;
     memcpy((void *) error.ptr, p, len);
-}
-
-static void principal_cb(const uint8_t *p, int len) {
-    principal.ptr = malloc(len);
-    principal.len = len;
-    memcpy((void *) principal.ptr, p, len);
-}
-
-static void text_cb(const uint8_t *p, int len) {
-    text.ptr = malloc(len);
-    text.len = len;
-    memcpy((void *) text.ptr, p, len);
 }
 
 int main(void) {
@@ -91,6 +80,7 @@ int main(void) {
     const char *url = "http://127.0.0.1:4943";
     const char *method = "greet";
     const char *method_args = "(\"World\")";
+    const char *method_args1 = "(\"Zondax\")";
 
     // Get did file content
     long file_size = get_did_file_size(did_file);
@@ -98,39 +88,37 @@ int main(void) {
     get_did_file_content(did_file, file_size, did_content);
 
     // Compute principal id from text
-    principal_from_text(id_text, principal_cb, error_cb);
+    PrincipalRet_u8 principal = principal_from_text(id_text,error_cb);
+    CHECK_ERROR(error);
 
-    // Compute anonymous id
-    id.ptr = malloc(ANONYMOUS_ID_LEN);
-    identity_anonymous(id.ptr);
+    //compute id
+    id.ptr = identity_anonymous();
     id.type = Anonym;
 
-    // Create Agent
-    const struct FFIAgent *agent_ptr = malloc(1000);
-    ResultCode result = agent_create(url, &id, &principal, did_content, &agent_ptr, error_cb);
-    if (result < 0) {
-        printf("%s\n", error.ptr);
-        return Err;
-    }
+    // Create Agent 1
+    FFIAgent *agent_1 = agent_create(url, &id, &principal, did_content, error_cb);
+    CHECK_ERROR(error);
+    char *call_1 = agent_query_wrap(agent_1, method, method_args, error_cb);
+    CHECK_ERROR(error);
 
-    // Send Update call
-    const void **update_ret = malloc(30);
-    result = agent_update(agent_ptr, method, method_args, update_ret, error_cb);
-    if (result < 0) {
-        printf("%s\n", error.ptr);
-        return Err;
-    }
 
-    // Translate idl result
-    idl_args_to_text(*update_ret, text_cb);
-    printf("%s\n", text.ptr);
+    // Create Agent 2
+    FFIAgent *agent_2 = agent_create(url, &id, &principal, did_content, error_cb);
+    CHECK_ERROR(error);
+    char *call_2 = agent_query_wrap(agent_2, method, method_args1,error_cb);
+    CHECK_ERROR(error);
+
+    //Translate idl result
+    char* text_1 = idl_args_to_text(call_1);
+    char* text_2 = idl_args_to_text(call_2);
+    printf("%s\n", text_1);
+    printf("%s\n", text_2);
 
     // Free Memory
     free(did_content);
     free((void *) error.ptr);
-    free((void *) principal.ptr);
-    free(id.ptr);
-    free((void *) agent_ptr);
-    free(update_ret);
+    principal_free(principal.ptr);
+    //identity_free(id.ptr);
+    //agent_free(agent_1);
     return 0;
 }
